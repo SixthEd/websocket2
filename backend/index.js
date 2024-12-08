16800169c433d247320ef9f696401bd39cd1395a
Guts <rohan.mahor92@gmail.com>
2024-12-08 15:03:51 +0530

    help

--- backend/index.js
+++ backend/index.js
@@ -1,80 +1,73 @@
 import express from "express";
 import { WebSocketServer } from "ws";
 
-
 const allowDomain = (req, res, next) => {
-    // res.header("Access-Control-Allow-Headers", "*"); 
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header('Access-Control-Allow-Credentials', "true");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    //
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, Content-Type");
     if (req.method.toLowerCase() === "options") return res.sendStatus(204);
     next();
-}
+};
 
-const heartBeatInterval = 30000;
+const heartBeatInterval = 1000 * 5; // 5 seconds
 const heartBeatValue = 1;
 
-function ping(ws) {
-    ws.send(heartBeatValue, { binary: true });
-}
+const ping = (ws) => {
+    ws.send(JSON.stringify({ binary: true }));
+};
+
 
 const app = express();
-
 app.use(allowDomain);
+app.use(express.static(".."));
 
-app.use(express.static("."));
+
 
 const server = app.listen(4000, () => {
-    console.log("server is running");
-})
+    console.log("Server is running on port 4000");
+});
 
-const web = new WebSocketServer({
-    noServer: true
-})
+const web = new WebSocketServer({ noServer: true });
 
 app.get("/chat", (req, res) => {
-
-    web.handleUpgrade(req, req.socket, new Buffer(""), (socket, incoming) => {
-
+    web.handleUpgrade(req, req.socket, req.headers, (socket) => {
         socket.isAlive = true;
+        
+        socket.send(JSON.stringify({ message: "WebSocket connected" }));
 
-        socket.send(JSON.stringify({ message: " websocket connected" }))
+        socket.on("close", (event) => {
+            console.log("WebSocket closing");
+            console.log(event)
+            // clearInterval(interval);
+        });
 
+        socket.on("message", (data) => {
+            data = JSON.parse(data);
 
-
-        socket.on("close", () => {
-            console.log(" websocket closing")
-        })
-
-        socket.on("message", (message, isBinary) => {
-            if (isBinary && message === heartBeatValue) {
-                console.log("pong");
-                socket.isAlive= true;
-            }   
-            else {
-                message = JSON.parse(message);
-                console.log(message);
+            if (data.message==="ping") {
+                console.log("Heartbeat received");
+                socket.isAlive = true;
+                socket.send(JSON.stringify({message: "pong"}))
+            } else {
+                console.log("Message:", data);
                 web.clients.forEach((client) => {
-                    if (client !== socket && client.readyState === socket.OPEN) {
-                        client.send(JSON.stringify(message))
+                    if (client !== socket && client.readyState === client.OPEN) {
+                        client.send(JSON.stringify(data));
                     }
-                })
+                });
             }
+        });
+    });
+});
 
-        })
-    })
-})
-
-const interval = setInterval(() => {
-    web.clients.forEach((client) => {
-        console.log("firing interval")
-        if (!client.isAlive) {
-            client.terminate();
-            return;
-        }
-        client.isAlive = false;
-        ping(client)
-    })
-}, heartBeatInterval)
+// const interval = setInterval(() => {
+//     web.clients.forEach((client) => {
+//         if (!client.isAlive) {
+//             client.terminate();
+//             return;
+//         }
+//         client.isAlive = false;
+//         ping(client);
+//     });
+// }, heartBeatInterval);
